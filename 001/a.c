c9cec604575ceef6d17f3594f9bca37c60d31228
okuguchihonoka <honoka.happy.0613@icloud.com>
2018-07-08 02:12:07 +0900

    簡略化

--- 001/a.c
+++ 001/a.c
@@ -5,12 +5,7 @@ int main(void){
   int a,b;
 
   scanf("%d\n%d",&a,&b);
-  
-  if(a >= 0 && a <= 2000){
-    if(b >= 0 && b <= 2000){
-      printf("%d\n",a - b);
-    }
-  }
-  
+  printf("%d\n",a - b);
+
   return 0;
 }
