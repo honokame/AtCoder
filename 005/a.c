525e2bb44bf9c054f41b1f2f0d20339e80e0b9c6
okuguchihonoka <honoka.happy.0613@icloud.com>
2018-07-08 02:16:59 +0900

    簡略化

--- 005/a.c
+++ 005/a.c
@@ -4,10 +4,7 @@ int main(void){
   int x,y;
 
   scanf("%d\t%d",&x,&y);
-  if(x >= 0 && x <= 100){
-    if(y >= 0 && y <= 100){
-      printf("%d\n",y / x);
-    }
-  }
+  printf("%d\n",y / x);
+
   return 0;
 }
