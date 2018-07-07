6ced5714300b47201b053a81e00d6dce5a240c10
okuguchihonoka <honoka.happy.0613@icloud.com>
2018-07-08 02:09:57 +0900

    簡略化

--- 002/a.c
+++ 002/a.c
@@ -3,17 +3,14 @@
 int main(void){
 
   int x,y;
+
   scanf("%d\t%d",&x,&y);
 
   if(x > y){
-    if(x >= 0 && y <= 1000000000){
       printf("%d\n",x);
-    }
   }
-  if(y > x){
-    if(x >= 0 && y <= 1000000000){
-      printf("%d\n",y);
-    }
+  else{
+    printf("%d\n",y);
   }
 
   return 0;
