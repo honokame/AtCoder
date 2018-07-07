cca90f10907186a87d03d12cab4c81e51d1ba289
okuguchihonoka <honoka.happy.0613@icloud.com>
2018-07-08 02:14:20 +0900

    簡略化

--- 003/a.c
+++ 003/a.c
@@ -5,12 +5,11 @@ int main(void){
 
   int N,sum = 0,i = 0;
   scanf("%d",&N);
-  if(N >= 4 && N <= 100){
-    for(i = 1;i <= N;i++){
-      sum += i;
-    }
-  printf("%d\n",(sum*10000) / N);
-  }
 
-  return 0;
+  for(i = 1;i <= N;i++){
+    sum += i;
+  }
+  printf("%d\n",(sum*10000) / N);
+
+return 0;
 }
