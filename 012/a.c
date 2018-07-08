0745398368e9bbd27c4e1824495b1e4c943e5f80
okuguchihonoka <honoka.happy.0613@icloud.com>
2018-07-08 13:27:25 +0900

    add new file

---
+++ 012/a.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void){
+
+  int a,b;
+
+  scanf("%d\t%d",&a,&b);
+  printf("%d\t%d",b,a);
+
+  return 0;
+}
+
---
+++ a.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(void){
+
+  int a,b;
+
+  scanf("%d\t%d",&a,&b);
+  printf("%d %d\n",b,a);
+
+  return 0;
+}
